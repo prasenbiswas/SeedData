3fffc1228ae04329e8135cb8572f8685eeb32a10
Prasen Biswas <73785918+prasenbiswas@users.noreply.github.com>
2022-12-20 00:51:10 +0530

    Add files via upload

---
+++ SeedData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeedDataPrac1.Models
+{
+    public class SeedData
+    {
+        [Key]
+        public int SeedId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }    
+    }
+}
---
+++ SeedDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SeedDataPrac1.Models
+{
+    public class SeedDbContext : DbContext
+    {
+        public SeedDbContext(DbContextOptions<SeedDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<SeedData> seedDatas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SeedData>().HasData(
+                new SeedData() { SeedId = 1, Name = "Prasen", Gender = "Male", Age = 24 },
+                new SeedData() { SeedId = 2, Name = "Biswas", Gender = "Male", Age = 25 }
+                );
+        }
+    }
+}
